4f3247fbe07b778e6a42e4baf93bdb4b3ec5965e
Devin Sharp <sharpdevin467@gmail.com>
2024-10-29 20:30:44 -0700

    server file adjustment

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,11 @@ dotenv.config();
 
 // Import the routes
 import routes from './routes/index.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -13,11 +18,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('../src/dist'));
+app.use(express.static("../client/dist"));
+//app.use(express.static('../src/dist'));
 
 
 // TODO: Implement middleware to connect the routes
 app.use(routes);
+app.get('/*', function (_req, res) {
+    res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
+});
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
